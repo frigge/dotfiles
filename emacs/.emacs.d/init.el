264bf44668f13e434473dfe95d7d9d07d4926edb
Sascha Fricke <fricke@freeman.cg.cs.tu-bs.de>
2020-02-19 20:15:53 +0100

    straigt/use-package fix

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,11 +4,25 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-    (package-install 'use-package))
-(setq use-package-always-ensure t)
+;; ;; Bootstrap `use-package'
+;; (unless (package-installed-p 'use-package)
+;; 	(package-refresh-contents)
+;;     (package-install 'use-package))
+;; (setq use-package-always-ensure t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
